c76cb18534a695186fadb03ff920d2f2e90b5df1
Cyane L <164072707+CyaneL@users.noreply.github.com>
2024-04-04 00:42:19 -0400

    Co-authored-by: Joy(Zhijiao) <lovelyjoy1991@users.noreply.github.com>

--- background.js
+++ background.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
   chrome.storage.local.get(["userName"], function (result) {
     if (result.userName) {
       nameField.value = result.userName;
-      greetingDiv.textContent = `Hello, ${result.userName}!`;
+      greetingDiv.textContent = `Hello, ${result.userName}! Please remember:`;
     }
   });
 
@@ -20,18 +20,18 @@ document.addEventListener("DOMContentLoaded", function () {
   saveButton.addEventListener("click", function () {
     const userName = nameField.value;
     chrome.storage.local.set({ userName: userName }, function () {
-      greetingDiv.textContent = `Hello, ${userName}!`;
+      greetingDiv.textContent = `Hello, ${userName}! Please remember:`;
       document.querySelector();
     });
   });
   // reset the name input when click reset button
   resetButton.addEventListener("click", function () {
-    nameField.value = undefined;
-
-  }
-  )
-
-
+    const userName = nameField.value;
+    chrome.storage.local.set({ userName: null }, function () {
+      nameField.value = null;
+      greetingDiv.textContent = `Hello, please tell me your name!`;
+    });
+  });
 
   // url constructor
   const memTextArr = [
@@ -40,13 +40,16 @@ document.addEventListener("DOMContentLoaded", function () {
     "you_are_picked_to_be_here",
     "you_are_so_qualified",
     "you_are_a_strong_and_capable_person",
-    
   ];
+
+  const imagesArr = [
+    "https://static01.nyt.com/images/2013/06/16/movies/20130616_MONSTER-slide-UCE2/20130616_MONSTER-slide-UCE2-master1050.jpg?",
+    "https://bpic.51yuansu.com/pic3/cover/01/90/73/59824ca7bfb3f_610.jpg?"
+  ]
 
   const basicUrl = "https://api.memegen.link/images/custom/_/";
   let memText = memTextArr[Math.floor(Math.random() * memTextArr.length)];
-  let imageUrl =
-    "https://static01.nyt.com/images/2013/06/16/movies/20130616_MONSTER-slide-UCE2/20130616_MONSTER-slide-UCE2-master1050.jpg?";
+  let imageUrl = imagesArr[Math.floor(Math.random() * imagesArr.length)]
   const imageSize = `height=450&width=800`;
   const assembledUrl = `${basicUrl}${memText}.jpg?background=${imageUrl}${imageSize}`;
   console.log(assembledUrl);
